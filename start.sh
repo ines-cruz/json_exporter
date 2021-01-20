b0e46d9c8c0d295660d3cbc79eb4c487d3f13a19
ines <inesruud7@gmail.com>
2021-01-20 14:06:58 +0100

    Adding changes to script to test docker

--- start.sh
+++ start.sh
@@ -5,17 +5,26 @@ trap '{ echo "sigint"; keepgoing=0; }' SIGINT
 
 cd ..
 cd  prometheus-2.22.2.linux-amd64
-
+cp  home/ines/json_exporter/examples/prometheus.yml cerncontainer:/prometheus.yml
+sleep 60
 ./prometheus --web.listen-address="0.0.0.0:9090" &
-
 
 cd ..
 cd json_exporter
+cp /home/ines/Downloads/billingcern.json cerncontainer:/billingcern.json
+
 make build
+
 python -m SimpleHTTPServer 8080 &
+  BACK_PID=$!
+wait $BACK_PID
 
 while (( keepgoing )); do
-  ./json_exporter http://localhost:8080/examples/output.json examples/config.yml &
- curl http://localhost:7979/probe?target=http://localhost:8080/examples/output.json
- sleep 86400 #once per day
+
+  ./json_exporter --config.file examples/config.yml &
+  BACK_PID=$!
+wait $BACK_PID
+
+ curl -k "http://localhost:7979/probe?target=http://localhost:8080/examples/output.json"
+ sleep 300 #once per day
 done
