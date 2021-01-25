df3519473eeff6f5598a604e72e0d1ce81bf9d34
ines <inesruud7@gmail.com>
2021-01-25 08:52:03 +0100

    Adding grafana and prometheus to docker compose Erasing prometheus commands from Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -25,13 +25,8 @@ COPY . /go/src/json_exporter/
 RUN go build -o json_exporter
 
 RUN chmod 777 -R json_exporter
-RUN cd ~ && cd /go/src &&  wget https://github.com/prometheus/prometheus/releases/download/v2.22.2/prometheus-2.22.2.linux-amd64.tar.gz && tar -xf prometheus-*.tar.gz
 
 
-RUN cd . &&  cp  /go/src/json_exporter/examples/prometheus.yml /go/src/prometheus-2.22.2.linux-amd64/prometheus.yml
-
-
-EXPOSE 9090 8080 7979
 
 ADD start.sh /
 RUN chmod +x /start.sh
--- start.sh
+++ start.sh
@@ -3,30 +3,19 @@
 keepgoing=1
 trap '{ echo "sigint"; keepgoing=0; }' SIGINT
 
-cd ..
-cd  prometheus-2.22.2.linux-amd64
-cp /home/ines/json_exporter/examples/prometheus.yml cerncontainer:/prometheus.yml 
 
-./prometheus --web.listen-address="0.0.0.0:9090" &
 
 cd ..
 cd json_exporter
-cp /home/ines/Downloads/billingcern.json cerncontainer:/billingcern.json
 
-make build
-while kill -0 $BACK_PID ; do
+export GOOGLE_APPLICATION_CREDENTIALS="/go/src/json_exporter/examples/billingcern.json"
+
 python -m SimpleHTTPServer 8080 &
-python -m SimpleHTTPServer 7979 &
- echo "Process is still active..."
-    sleep 1
-done
+
 while (( keepgoing )); do
-while kill -0 $BACK_PID ; do
 
  ./json_exporter http://localhost:8080/examples/output.json examples/config.yml &
- echo "Process is still active222..."
-    sleep 30
-done
+
 
  curl -k "http://localhost:7979/probe?target=http://localhost:8080/examples/output.json"
  sleep 300
