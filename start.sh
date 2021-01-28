8c50270aecb74a9478c0472bd81af0b2e3ca5790
ines <inesruud7@gmail.com>
2021-01-28 10:20:43 +0100

    First try with git-crypt after adding collab

--- start.sh
+++ start.sh
@@ -8,10 +8,11 @@ trap '{ echo "sigint"; keepgoing=0; }' SIGINT
 cd ..
 cd json_exporter
 
-export GOOGLE_APPLICATION_CREDENTIALS="/go/src/json_exporter/examples/billingcern.json"
+export GOOGLE_APPLICATION_CREDENTIALS="/go/src/json_exporter/billingcern.json"
 #export GOOGLE_APPLICATION_CREDENTIALS="/home/ines/Downloads/billingcern.json"
 
 python -m SimpleHTTPServer 8080 &
+python -m SimpleHTTPServer 7979 &
 
 while (( keepgoing )); do
 
@@ -19,5 +20,5 @@ while (( keepgoing )); do
 
 
  curl -k "http://localhost:7979/probe?target=http://localhost:8080/examples/output.json"
- sleep 172800s #twice per day
+ sleep 3600s
 done
